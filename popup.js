9fea620f04ab5734999f9580def2c8e7f0a8c045
Brett Commandeur <bcmdr.co@gmail.com>
2022-05-08 21:24:06 -0600

    refactor

--- popup.js
+++ popup.js
@@ -1,99 +1,121 @@
-function getStorms() {
-  async function getData(url = '') {
-    const response = await fetch(url, {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-API-Key': apiKey
-      }
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
-  }
-  getData('https://api.stormboard.com/storms')
-  .then(data => {
-    let stormList = [...data.storms];
-    let select = document.getElementById('stormSelect');
+async function getData(url = '') {
+  const response = await fetch(url, {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-Key': apiKey
+    }
+  });
+  return response.json();
+}
+
+async function postData(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-Key': apiKey
+    },
+    body: JSON.stringify(data)
+  });
+  return response.json();
+}
+
+async function getStorms() {
+  try {
+    let response = await getData('https://api.stormboard.com/storms')
+    let stormList = [...response.storms];
+    let select = document.getElementById('storm-select');
+
+    // Build the Storm Select Drop-down
     for (const storm of stormList) {
       let option = document.createElement('option');
       option.value = storm.id;
       option.innerText = storm.title;
       select.append(option);
     }
-  }).catch((error) => {
+
+  } catch (error) {
+    console.error(error);
+    // Redirect to API Docs to work-around CORS. 
     chrome.tabs.update({
       url: "https://api.stormboard.com/docs"
     });
-  });
+  }
 }
 
-function saveIdea(url) {
-  async function postData(url = '', data = {}, apiKey = '') {
-    const response = await fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-API-Key': apiKey
-      },
-      body: JSON.stringify(data)
-    });
-    return response.json();
-  }
-
-  let ideaText = document.getElementById("idea-text")?.value;
-  let stormid = document.getElementById("stormSelect").value;
+async function saveIdea(url) {
+  let textArea = document.getElementById("idea-text");
+  let ideaText = textArea.value;
+  let stormid = document.getElementById("storm-select").value;
   let includeLink = document.getElementById('include-link');
   
+  // If idea is empty, focus the text area and stop. 
   if (ideaText.trim() === '') {
-    let textArea = document.getElementById("idea-text");
     textArea.focus();
     textArea.value = '';
     return; 
   }
 
+  // Append the current page's url to the idea if selected. 
   ideaText = includeLink.checked ? ideaText + `\n\n${url}` : ideaText;
   
-  let data = {
+  // Create the idea. 
+  let idea = {
     stormid,
     data: ideaText,
     x: 100,
     y: 250,
   }
 
-  postData('https://api.stormboard.com/ideas', data, apiKey)
-  .then(data => {
-    if (data.status === 200) {
-      let saveIdea = document.getElementById('saveIdea');
-      saveIdea.classList.add('btn-saved');
-      saveIdea.innerText = 'Saved';
-      let textArea = document.getElementById("idea-text");
-      textArea.classList.add('saved');
-      textArea.addEventListener('click', () => {
-        textArea.value = '';
-        textArea.classList.remove('saved');
-        saveIdea.innerText = 'Save Idea';
-        saveIdea.classList.remove('btn-saved');
-      });
-    }
-  });
+  // Submit the idea.
+  let response = await postData('https://api.stormboard.com/ideas', idea);
+
+  // Response was successful.
+  if (response.status === 200) {
+
+    // Change the save button to show it saved. 
+    let saveIdea = document.getElementById('saveIdea');
+    saveIdea.classList.add('btn-saved');
+    saveIdea.innerText = 'Saved';
+
+    // Change the text area to show it saved. 
+    let textArea = document.getElementById("idea-text");
+    textArea.classList.add('saved');
+
+    // Add click listener to revert saved status. 
+    textArea.addEventListener('click', () => {
+      textArea.value = '';
+      textArea.classList.remove('saved');
+      saveIdea.innerText = 'Save Idea';
+      saveIdea.classList.remove('btn-saved');
+    });
+  }
 }
 
-getStorms();
-let saveIdeaBtn = document.getElementById("saveIdea");
-let goToStormBtn = document.getElementById("goToStorm");
-let textArea = document.getElementById("idea-text");
-textArea.focus();
+// Run main function when extension pop-up loads.
+(function main() {
+  let textArea = document.getElementById("idea-text");
+  let saveIdeaBtn = document.getElementById("saveIdea");
+  let goToStormBtn = document.getElementById("goToStorm");
+  
+  // Fetch Storms from Stormboard API and ready the text area. 
+  getStorms();
+  textArea.focus();
 
-saveIdeaBtn.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  saveIdea(tab.url);
-});
-
-goToStormBtn.addEventListener('click', async () => {
-  // let [tab] = await chrome.tabs.query({ active: true, currentWindow: true});
-  let stormid = document.getElementById("stormSelect").value;
-  chrome.tabs.update({
-    url: "http://www.stormboard.com/storm/" + stormid
+  // Save Idea Button
+  saveIdeaBtn.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    saveIdea(tab.url);
   });
-});
+
+  // Go to Storm Button
+  goToStormBtn.addEventListener('click', async () => {
+    let stormid = document.getElementById("storm-select").value;
+    chrome.tabs.update({
+      url: "http://www.stormboard.com/storm/" + stormid
+    });
+  });
+})(); 
