99099fff6d727137a034bfa0bb872bd3ce3747ff
Brett Commandeur <bcommandeur@gmail.com>
2022-12-02 08:31:22 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,1 @@
-// let apiKey = '39a63810f70e8f1a0e7b5421f1370365998f6e9c';
 
-// chrome.runtime.onInstalled.addListener(() => {
-//   console.log(apiKey);
-//   chrome.storage.sync.set({apiKey: apiKey});
-// });
